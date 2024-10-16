ae5b530dd62fbd3ba532cee1d7b7743806f32532
Szaszuge <78954822+Szaszuge@users.noreply.github.com>
2024-10-16 09:19:59 +0200

    modulok require

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const mysql = require('mysql'); // MySQL modul importálása
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use('/assets', express.static('assets'));
 
-var mysql      = require('mysql');
+
 var connection = mysql.createConnection({
   host     : process.env.DBHOST,
   user     : process.env.DBUSER,
