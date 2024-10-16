1ba7af16e105b63d4b9ca9d606d31aa6fc7700ed
Szaszuge <78954822+Szaszuge@users.noreply.github.com>
2024-10-16 12:01:22 +0200

    jó align

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 async function fetchBooks() {
-    const response = await fetch('http://localhost:5000/books');
+    const response = await fetch('http://localhost:5000/books'); // Győződj meg róla, hogy a backend a megfelelő adatokat küldi.
     const books = await response.json();
     const bookList = document.getElementById('book-list');
-    bookList.innerHTML = '';
-    books.forEach(books => {
-        const div = document.createElement('div');
-        div.textContent = `${books.book_id} ${books.title} (${books.publish_year}) - ISBN: ${books.isbn}`;
-        bookList.appendChild(div);
+    bookList.innerHTML = `<tr>
+    <th >#</th>
+    <th >Cím</th>
+    <th >Szerző</th>
+    <th >Kiadás éve</th>
+    <th >ISBN szám</th>
+</tr>`;
+    
+    books.forEach(book => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${book.book_id}</td>
+            <td class="book-title">${book.title}</td>
+            <td>${book.author}</td> <!-- Itt már elérheted az author-t -->
+            <td>${book.publish_year}</td>
+            <td class="book-isbn">${book.isbn}</td>
+        `;
+        bookList.appendChild(row);
     });
 }
 
