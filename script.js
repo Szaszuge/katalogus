6dc261fc0f445a6ad29f2821d819c3d6475cb0c7
Szaszuge <78954822+Szaszuge@users.noreply.github.com>
2024-10-20 21:30:33 +0200

    fetch helyett xmlhttprequest

--- script.js
+++ script.js
@@ -1,37 +1,63 @@
-async function fetchBooks() {
-    const response = await fetch('http://localhost:5000/books'); 
-    const books = await response.json();
-    const bookList = document.getElementById('book-list');
-    bookList.innerHTML = `<tr>
-    <th >#</th>
-    <th >Cím</th>
-    <th >Szerző</th>
-    <th >Kiadás éve</th>
-    <th >ISBN szám</th>
-</tr>`;
-    
-    books.forEach(book => {
-        const row = document.createElement('tr');
-        let authors = fetchAuthors(book.book_id);
-        console.log(`authors`)
-        row.innerHTML = `
-            <td>${book.book_id}</td>
-            <td class="book-title">${book.title}</td>
-            <td>${authors}</td>
-            <td>${book.publish_year}</td>
-            <td class="book-isbn">${book.isbn}</td>
-        `;
-        bookList.appendChild(row);
-    });
+function fetchBooks() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:5000/books', true);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            const books = JSON.parse(xhr.responseText);
+            const bookList = document.getElementById('book-list');
+            bookList.innerHTML = `<tr>
+                <th>#</th>
+                <th>Cím</th>
+                <th>Szerző</th>
+                <th>Kiadás éve</th>
+                <th>ISBN szám</th>
+            </tr>`;
+            
+            books.forEach(book => {
+                const row = document.createElement('tr');
+                fetchAuthors(book.book_id, function(authors) {
+                    row.innerHTML = `
+                        <td>${book.book_id}</td>
+                        <td class="book-title">${book.title}</td>
+                        <td>${authors}</td>
+                        <td>${book.publish_year}</td>
+                        <td class="book-isbn">${book.isbn}</td>
+                    `;
+                    bookList.appendChild(row);
+                });
+            });
+        } else {
+            console.error('Error fetching books');
+        }
+    };
+    xhr.onerror = function () {
+        console.error('Request error');
+    };
+    xhr.send();
 }
-async function fetchAuthors(bookID) {
-    const response = await fetch(`http://localhost:5000/authorsOf/${bookID}`); 
-    const authors = await response.json();
-    let valasz = "";
-    authors.forEach(author => {
-        valasz += `${author.name}, `
-    })
-    console.log(`${valasz}`);
-    return (valasz);
+
+function fetchAuthors(bookID, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `http://localhost:5000/authorsOf/${bookID}`, true);
+    xhr.onload = function () {
+        if (xhr.status === 202 || xhr.status === 200) {
+            const authors = JSON.parse(xhr.responseText);
+            let authorNames = "";
+            authors.forEach(author => {
+                authorNames += `${author.name}, `;
+            });
+            callback(authorNames.slice(0, -2)); // Remove the last comma and space
+        } else if (xhr.status === 203) {
+            callback('Nincsenek szerzők');
+        } else {
+            console.error('Error fetching authors');
+        }
+    };
+    xhr.onerror = function () {
+        console.error('Request error');
+    };
+    xhr.send();
 }
+
+// Call the function to fetch books
 fetchBooks();
